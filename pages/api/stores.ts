6074d86a94e661c1c7c99c6356e48c2c0ecc2544
rkdghwnd <bsc6448@naver.com>
2023-07-17 18:12:15 +0900

    section4

--- pages/api/stores.ts
+++ pages/api/stores.ts
@@ -5,6 +5,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Store[]>
 ) {
+  // 장점 : 실제 백엔드 URL을 브라우저에서 노출시키지 않을 수 있다.
   const stores = (await import('../../public/stores.json')).default as Store[];
 
   res.status(200).json(stores);
